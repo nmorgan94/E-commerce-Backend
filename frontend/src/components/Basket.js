0fcdbc98d49986726eec6b9ce66186579651900d
Nick Morgan <nickgmorgan94@gmail.com>
2020-03-11 13:41:19 +0000

    refactor basket

--- frontend/src/components/Basket.js
+++ frontend/src/components/Basket.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { observer, inject } from "mobx-react";
 import { API_BASE_URL } from "../constants";
 import Cookies from "universal-cookie";
-import { BasketList } from "./BasketList";
 
 const cookies = new Cookies();
 
@@ -12,10 +11,29 @@ export const Basket = inject("dataStore")(
       dataStore.getBasket();
     }, []);
 
+    const items = dataStore.basketContent.map(item => (
+      <div className="card red" key={item.product.id}>
+        <div className="">
+          <div className="card-image">
+            <img src={item.product.pictureUrl} alt={item.product.name} />
+            <span className="card-title">{item.product.name}</span>
+          </div>
+          <div className="card-content row white">
+            <div className="col s6">
+              <b>Price: £{item.product.price}</b>
+            </div>
+            <div className="col s6">
+              <b>Quantity: {item.quantity}</b>
+            </div>
+          </div>
+        </div>
+      </div>
+    ));
+
     return (
       <div>
-        <BasketList />
         <div className="container grey">
+          <div>{items}</div>
           <div className="">Subtotal: £{dataStore.basket.basketPrice}</div>
         </div>
       </div>
